4350cdcf9b619b3b24d8c061ae02da4ed10b4c67
Sean Urbassik <sean.urbassik@gmail.COM>
2019-07-15 14:22:56 -0400

    started models and add google maps depends

--- spot_me/models.py
+++ spot_me/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=1000)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Gym(models.Model):
+    name = models.CharField(max_length=100)
+    info = models.CharField(max_length=1000)
--- spot_me_django/settings.py
+++ spot_me_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'spot_me',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'spot_me_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spot_me',
+        'USER': 'spotmeuser',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost'
     }
 }
 
