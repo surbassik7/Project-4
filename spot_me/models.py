a9a35a9e2e1c028196da00e72b5c98b2b9ab8ff2
Sean Urbassik <sean.urbassik@gmail.COM>
2019-07-19 14:58:26 -0400

    all api data pulls

--- spot_me/models.py
+++ spot_me/models.py
@@ -21,15 +21,6 @@ class Gym(models.Model):
         return self.name
 
 
-class Search(models.Model):
-    term = models.CharField(max_length=255, null=True, blank=True)
-    position = gis_models.PointField(srid=4326, dim=2)
-    results_count = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return "{} ({})".format(self.term, self.position)
-
 
 from django.db import models
 
--- spot_me/search.py
+++ spot_me/search.py
@@ -3,13 +3,48 @@ import requests
 import googlemaps
 from googlemaps import Client
 
-locationrequest = requests.get('http://api.ipstack.com/check?access_key=3fea3b33a38ef7abd674c9e0515183be&fields=latitude,longitude')
-location_text = locationrequest.text
-location = json.loads(location_text)
-print(location)
+check_url = 'http://api.ipstack.com/check?access_key=3fea3b33a38ef7abd674c9e0515183be'
+r = requests.get(check_url)
+j = json.loads(r.text)
+lat = j['latitude']
+lon = j['longitude']
+mylocation = (','.join([str(lat), str(lon)]))
+
+print(mylocation)
 
 
-gmaps = Client(key='AIzaSyDOwVK7bGap6b5Mpct1cjKMp7swFGi3uGg')
+BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+LOCATION = mylocation
+RADIUS = '2500'
+TYPE = 'gym'
+API_KEY = 'AIzaSyDOwVK7bGap6b5Mpct1cjKMp7swFGi3uGg'
+KEYWORDS = ''
 
-list_all_gyms = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters')
-print(list_all_gyms)
+allgyms = requests.get(BASE_URL+'location='+LOCATION+'&radius='+RADIUS+'&type='+TYPE+'&key='+API_KEY)
+all_text = allgyms.text
+alljson = json.loads(all_text)
+
+KEYWORDS = 'healthclub'
+healthclubs = requests.get(BASE_URL+'location='+LOCATION+'&radius='+RADIUS+'&type='+TYPE+'&keyword='+KEYWORDS+'&key='+API_KEY) 
+health_text = healthclubs.text 
+healthjson = json.loads(health_text)
+
+
+KEYWORDS = 'crossfit'
+crossfit = requests.get(BASE_URL+'location='+LOCATION+'&radius='+RADIUS+'&type='+TYPE+'&keyword='+KEYWORDS+'&key='+API_KEY) 
+cross_text = crossfit.text 
+crossjson = json.loads(cross_text)
+
+
+KEYWORDS = '(martial) AND (arts)'
+martialarts = requests.get(BASE_URL+'location='+LOCATION+'&radius='+RADIUS+'&type='+TYPE+'&keyword='+KEYWORDS+'&key='+API_KEY) 
+martial_text = martialarts.text 
+martialjson = json.loads(martial_text)
+
+
+KEYWORDS = '(yoga) AND (pilates)'
+yoga = requests.get(BASE_URL+'location='+LOCATION+'&radius='+RADIUS+'&type='+TYPE+'&keyword='+KEYWORDS+'&key='+API_KEY) 
+yoga_text = yoga.text 
+yogajson = json.loads(yoga_text)
+
+print(type(healthjson))
